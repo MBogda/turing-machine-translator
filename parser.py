01cff9fc213aec6ae7de1c1b9ae9ba26eab69aa9
Bogdan Markov <bogdick@yandex.ru>
2017-06-10 22:35:28 +0300

    Parser: trying to handle expressions

--- parser.py
+++ parser.py
@@ -35,12 +35,24 @@ class Parser:
                     self.token.type),
                 self.token.line, self.token.column)
 
+    # todo? add one-line if, while
     def if_(self):
         self.accept(Token.IF)
         self.boolean_expression()
         self.accept(Token.COLON)
-        self.accept(Token.NEWLINE)  # todo? add one-line if
+        self.accept(Token.NEWLINE)
         self.block_of_instructions()
+        while self.token.type == Token.ELIF:
+            self.accept(Token.ELIF)
+            self.boolean_expression()
+            self.accept(Token.COLON)
+            self.accept(Token.NEWLINE)
+            self.block_of_instructions()
+        if self.token.type == Token.ELSE:
+            self.accept(Token.ELSE)
+            self.accept(Token.COLON)
+            self.accept(Token.NEWLINE)
+            self.block_of_instructions()
 
     def while_(self):
         self.accept(Token.WHILE)
@@ -49,7 +61,12 @@ class Parser:
         self.accept(Token.NEWLINE)
         self.block_of_instructions()
 
-    def block_of_instructions(self): pass
+    def block_of_instructions(self):
+        self.accept(Token.INDENT)
+        self.instruction()
+        while self.token.type != Token.DEDENT:
+            self.instruction()
+        self.accept(Token.DEDENT)
 
     def output(self):
         if self.token.type == Token.OUTPUT_BOOLEAN:
@@ -64,7 +81,7 @@ class Parser:
         elif self.token.type == Token.OUTPUT_TAPE:
             self.accept(Token.OUTPUT_TAPE)
             self.tape_expression()
-        elif self.token.type == Token.OUTPUT_ANY:   # ???
+        elif self.token.type == Token.OUTPUT_ANY:  # ???
             self.accept(Token.OUTPUT_ANY)
             self.any_expression()
 
@@ -77,7 +94,24 @@ class Parser:
         self.accept(Token.ASSIGNMENT)
         # todo ...
 
-    def boolean_expression(self): pass
+    def boolean_expression(self):
+        self.boolean_term()
+        while self.token.type == Token.OR:
+            self.accept(Token.OR)
+            self.boolean_term()
+
+    def boolean_term(self):
+        self.boolean_multiplier()
+        while self.token.type == Token.AND:
+            self.accept(Token.AND)
+            self.boolean_multiplier()
+
+    def boolean_multiplier(self):
+        self.integer_expression()
+        while self.token.type in [Token.EQUAL, Token.NOT_EQUAL, Token.LESS, Token.GREATER,
+                                  Token.LESS_OR_EQUAL, Token.GREATER_OR_EQUAL]:
+            self.accept(self.token.type)
+            self.integer_expression()
 
     def integer_expression(self):
         self.integer_term()
@@ -97,13 +131,17 @@ class Parser:
 
         if self.token.type == Token.IDENTIFIER:
             self.accept(Token.IDENTIFIER)
+            if self.token.type == Token.LEFT_SQUARE_BRACKET:
+                self.accept(Token.LEFT_SQUARE_BRACKET)
+                self.accept(Token.RIGHT_SQUARE_BRACKET)
         elif self.token.type == Token.INTEGER_LITERAL:
             self.accept(Token.INTEGER_LITERAL)
         elif self.token.type == Token.INPUT_INTEGER:
             self.accept(Token.INPUT_INTEGER)
         elif self.token.type == Token.LEFT_BRACKET:
             self.accept(Token.LEFT_BRACKET)
-            self.integer_expression()
+            # self.integer_expression()
+            self.boolean_expression()
             self.accept(Token.RIGHT_BRACKET)
         else:
             generate_error(
@@ -112,11 +150,14 @@ class Parser:
                     self.token.type),
                 self.token.line, self.token.column)
 
-    def symbol_expression(self): pass
+    def symbol_expression(self):
+        pass
 
-    def tape_expression(self): pass
+    def tape_expression(self):
+        pass
 
-    def any_expression(self): pass
+    def any_expression(self):
+        pass
 
     def parse(self):
         self.integer_expression()
