36a6c5d080435cd6d0fab2721627db49f83cfb54
Bogdan Markov <bogdick@yandex.ru>
2017-06-11 10:19:44 +0300

    Parser: handle expressions without type
    + refactor errors
    + handle turing machine literals and changes

--- parser.py
+++ parser.py
@@ -7,12 +7,21 @@ class Parser:
         self.lexer = lexer
         self.token = lexer.next_token()
 
-    def accept(self, token_type):
-        if self.token.type == token_type:
+    def error_expected(self, expected):
+        if not isinstance(expected, (tuple, list)):
+            expected = (expected,)
+        generate_error('Parser', 'expected {}, got {}'.format(
+            ' or '.join(str(token) for token in expected), self.token.type
+        ), self.token.line, self.token.column)
+        self.token = Token(Token.END_OF_FILE, )
+
+    def accept(self, token_types):
+        if not isinstance(token_types, (tuple, tuple)):
+            token_types = (token_types,)
+        if self.token.type in token_types:
             self.token = self.lexer.next_token()
         else:
-            generate_error('Parser', 'expected token {}, got {}'.format(token_type, self.token.type),
-                           self.token.line, self.token.column)
+            self.error_expected(token_types)
 
     def program(self):
         while self.token:
@@ -20,31 +29,28 @@ class Parser:
 
     def instruction(self):
         if self.token.type == Token.IF:
-            self.if_()
+            self.if_statement()
         elif self.token.type == Token.WHILE:
-            self.while_()
-        elif self.token.type in [Token.OUTPUT_BOOLEAN, Token.OUTPUT_INTEGER, Token.OUTPUT_SYMBOL,
-                                 Token.OUTPUT_TAPE, Token.OUTPUT_ANY]:
-            self.output()
+            self.while_statement()
+        elif self.token.type in (Token.OUTPUT_BOOLEAN, Token.OUTPUT_INTEGER, Token.OUTPUT_SYMBOL,
+                                 Token.OUTPUT_TAPE, Token.OUTPUT_ANY):
+            self.output_statement()
         elif self.token.type == Token.IDENTIFIER:
             self.assignment()
         else:
-            generate_error(
-                'Parser',
-                'expected IF or WHILE or OUTPUT or IDENTIFIER, got {}'.format(
-                    self.token.type),
-                self.token.line, self.token.column)
+            self.error_expected((Token.IF, Token.WHILE, Token.OUTPUT_BOOLEAN, Token.OUTPUT_INTEGER, Token.OUTPUT_SYMBOL,
+                                 Token.OUTPUT_TAPE, Token.OUTPUT_ANY, Token.IDENTIFIER))
 
     # todo? add one-line if, while
-    def if_(self):
+    def if_statement(self):
         self.accept(Token.IF)
-        self.boolean_expression()
+        self.expression()
         self.accept(Token.COLON)
         self.accept(Token.NEWLINE)
         self.block_of_instructions()
         while self.token.type == Token.ELIF:
             self.accept(Token.ELIF)
-            self.boolean_expression()
+            self.expression()
             self.accept(Token.COLON)
             self.accept(Token.NEWLINE)
             self.block_of_instructions()
@@ -54,9 +60,9 @@ class Parser:
             self.accept(Token.NEWLINE)
             self.block_of_instructions()
 
-    def while_(self):
+    def while_statement(self):
         self.accept(Token.WHILE)
-        self.boolean_expression()
+        self.expression()
         self.accept(Token.COLON)
         self.accept(Token.NEWLINE)
         self.block_of_instructions()
@@ -68,100 +74,131 @@ class Parser:
             self.instruction()
         self.accept(Token.DEDENT)
 
-    def output(self):
-        if self.token.type == Token.OUTPUT_BOOLEAN:
-            self.accept(Token.OUTPUT_BOOLEAN)
-            self.boolean_expression()
-        elif self.token.type == Token.OUTPUT_INTEGER:
-            self.accept(Token.OUTPUT_INTEGER)
-            self.integer_expression()
-        elif self.token.type == Token.OUTPUT_SYMBOL:
-            self.accept(Token.OUTPUT_SYMBOL)
-            self.symbol_expression()
-        elif self.token.type == Token.OUTPUT_TAPE:
-            self.accept(Token.OUTPUT_TAPE)
-            self.tape_expression()
-        elif self.token.type == Token.OUTPUT_ANY:  # ???
-            self.accept(Token.OUTPUT_ANY)
-            self.any_expression()
+    def output_statement(self):
+        self.accept((Token.OUTPUT_BOOLEAN, Token.OUTPUT_INTEGER, Token.OUTPUT_SYMBOL,
+                     Token.OUTPUT_TAPE, Token.OUTPUT_ANY))
+        self.expression()
+        self.accept(Token.NEWLINE)
 
     def assignment(self):
         self.accept(Token.IDENTIFIER)
         if self.token.type == Token.LEFT_SQUARE_BRACKET:
             self.accept(Token.LEFT_SQUARE_BRACKET)
-            self.integer_expression()
+            self.expression()
             self.accept(Token.RIGHT_SQUARE_BRACKET)
-        self.accept(Token.ASSIGNMENT)
-        # todo ...
+        self.accept((Token.ASSIGNMENT, Token.ASSIGNMENT_PLUS, Token.ASSIGNMENT_MINUS, Token.ASSIGNMENT_MULTIPLY,
+                     Token.ASSIGNMENT_DIVIDE, Token.ASSIGNMENT_MODULO))
+        self.expression()
+        if self.token.type == Token.COLON:
+            self.turing_machine_change_commands()
+        else:
+            self.accept(Token.NEWLINE)
 
-    def boolean_expression(self):
-        self.boolean_term()
+    def expression(self):
+        self.subexpr1()
         while self.token.type == Token.OR:
             self.accept(Token.OR)
-            self.boolean_term()
+            self.subexpr1()
 
-    def boolean_term(self):
-        self.boolean_multiplier()
+    def subexpr1(self):
+        self.subexpr2()
         while self.token.type == Token.AND:
             self.accept(Token.AND)
-            self.boolean_multiplier()
+            self.subexpr2()
 
-    def boolean_multiplier(self):
-        self.integer_expression()
-        while self.token.type in [Token.EQUAL, Token.NOT_EQUAL, Token.LESS, Token.GREATER,
-                                  Token.LESS_OR_EQUAL, Token.GREATER_OR_EQUAL]:
+    def subexpr2(self):
+        while self.token.type == Token.NOT:
+            self.accept(Token.NOT)
+
+        self.subexpr3()
+        if self.token.type in (Token.EQUAL, Token.NOT_EQUAL, Token.LESS, Token.GREATER,
+                               Token.LESS_OR_EQUAL, Token.GREATER_OR_EQUAL):
             self.accept(self.token.type)
-            self.integer_expression()
+            self.subexpr3()
 
-    def integer_expression(self):
-        self.integer_term()
-        while self.token.type in [Token.PLUS, Token.MINUS]:
+    def subexpr3(self):
+        self.subexpr4()
+        while self.token.type in (Token.PLUS, Token.MINUS):
             self.accept(self.token.type)
-            self.integer_term()
+            self.subexpr4()
 
-    def integer_term(self):
-        self.integer_multiplier()
-        while self.token.type in [Token.MULTIPLY, Token.DIVIDE, Token.MODULO]:
+    def subexpr4(self):
+        self.subexpr5()
+        while self.token.type in (Token.MULTIPLY, Token.DIVIDE, Token.MODULO):
             self.accept(self.token.type)
-            self.integer_multiplier()
+            self.subexpr5()
 
-    def integer_multiplier(self):
-        if self.token.type == Token.MINUS:
+    def subexpr5(self):
+        self.term()
+        while self.token.type in (Token.HEAD_PLUS, Token.HEAD_MINUS, Token.HEAD_MULTIPLY,
+                                  Token.HEAD_DIVIDE, Token.HEAD_MODULO):
+            self.accept(self.token.type)
+            self.term()
+
+    def term(self):
+        while self.token.type == Token.MINUS:
             self.accept(Token.MINUS)
 
+        # identifiers
         if self.token.type == Token.IDENTIFIER:
             self.accept(Token.IDENTIFIER)
-            if self.token.type == Token.LEFT_SQUARE_BRACKET:
-                self.accept(Token.LEFT_SQUARE_BRACKET)
-                self.accept(Token.RIGHT_SQUARE_BRACKET)
-        elif self.token.type == Token.INTEGER_LITERAL:
-            self.accept(Token.INTEGER_LITERAL)
-        elif self.token.type == Token.INPUT_INTEGER:
-            self.accept(Token.INPUT_INTEGER)
+
+        # literals
+        elif self.token.type in (Token.TRUE, Token.FALSE, Token.INTEGER_LITERAL,
+                                 Token.SYMBOL_LITERAL, Token.TAPE_LITERAL):
+            self.accept(self.token.type)
+
+        # turing machine literal
+        elif self.token.type == Token.LEFT_BRACE:
+            self.turing_machine_literal()
+
+        # input statements
+        elif self.token.type in (Token.INPUT_BOOLEAN, Token.INPUT_INTEGER, Token.INPUT_SYMBOL, Token.INPUT_TAPE):
+            self.accept(self.token.type)
+
+        # subexpression in parentheses
         elif self.token.type == Token.LEFT_BRACKET:
             self.accept(Token.LEFT_BRACKET)
-            # self.integer_expression()
-            self.boolean_expression()
+            self.expression()
             self.accept(Token.RIGHT_BRACKET)
         else:
-            generate_error(
-                'Parser',
-                'expected IDENTIFIER or INTEGER_LITERAL or INPUT_OPERATOR or LEFT_BRACKET, got {}'.format(
-                    self.token.type),
-                self.token.line, self.token.column)
+            self.error_expected((Token.MINUS, Token.IDENTIFIER, Token.TRUE, Token.INTEGER_LITERAL, Token.SYMBOL_LITERAL,
+                                 Token.TAPE_LITERAL, Token.LEFT_BRACE, Token.INPUT_BOOLEAN, Token.INPUT_INTEGER,
+                                 Token.INPUT_SYMBOL, Token.INPUT_TAPE, Token.LEFT_BRACKET))
+            return
+        if self.token.type == Token.LEFT_SQUARE_BRACKET:
+            self.accept(Token.LEFT_SQUARE_BRACKET)
+            if self.token.type != Token.RIGHT_SQUARE_BRACKET:
+                self.expression()
+            self.accept(Token.RIGHT_SQUARE_BRACKET)
 
-    def symbol_expression(self):
-        pass
+    def turing_machine_literal(self):
+        self.accept(Token.LEFT_BRACE)
+        self.accept(Token.IDENTIFIER)
+        self.accept(Token.SYMBOL_LITERAL)
+        if self.token.type == Token.COLON:
+            self.accept(Token.COLON)
+            while self.token != Token.RIGHT_BRACE:
+                self.turing_machine_command()
+                self.accept(Token.COMMA)
+        self.accept(Token.RIGHT_BRACE)
 
-    def tape_expression(self):
-        pass
+    def turing_machine_change_commands(self):
+        self.accept(Token.COLON)
+        self.accept(Token.NEWLINE)
+        self.accept(Token.INDENT)
+        while self.token.type != Token.DEDENT:
+            self.turing_machine_command()
+            self.accept(Token.NEWLINE)
+        self.accept(Token.DEDENT)
 
-    def any_expression(self):
-        pass
+    def turing_machine_command(self):
+        self.accept(Token.IDENTIFIER)
+        self.accept(Token.SYMBOL_LITERAL)
+        self.accept(Token.ASSIGNMENT)
+        self.accept((Token.IDENTIFIER, Token.MINUS))
+        self.accept((Token.SYMBOL_LITERAL, Token.MINUS))
+        self.accept((Token.LESS, Token.GREATER, Token.MINUS))
 
     def parse(self):
-        self.integer_expression()
-        if self.token.type == Token.NEWLINE:
-            print('OK')
-        else:
-            generate_error('Parser', self.token, self.token.line, self.token.column)
+        self.program()
